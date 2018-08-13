07088bcb5c9125ea7881d426dbc4aea03e3969ef
Sharmina Akhtar <sakht318@gmail.com>
2018-08-13 11:51:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+function showRepositories(event, data) {
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText)
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
+}
+
+function showRepositories(event, data) {
+  console.log(this.responseText)
+  let repoList = "<ul>"
+  for(var i=0;i < this.responseText.length; i++) {
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  }
+  repoList += "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
 }
