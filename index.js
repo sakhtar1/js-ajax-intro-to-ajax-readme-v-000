e9ea698e2f0c8a451be9cd272b09d918f8cb20ff
Sharmina Akhtar <sakht318@gmail.com>
2018-08-13 12:00:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function showRepositories(event, data) {
   document.getElementById("repositories").innerHTML = repoList
 }
 
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.open("GET", 'https://api.github.com/users/octocat/repos')
