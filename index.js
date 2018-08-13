355de4c1d3e5238ab40f7eae867391b69b570da2
Sharmina Akhtar <sakht318@gmail.com>
2018-08-13 11:57:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 function showRepositories(event, data) {
   //this is set to the XMLHttpRequest object that fired the event
-  console.log(this.responseText)
-  let repoList = "<ul>"
-  for(var i=0;i < this.responseText.length; i++) {
-    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-  }
-  repoList += "</ul>"
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
@@ -15,3 +12,10 @@ function getRepositories() {
   req.send()
 }
 
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
